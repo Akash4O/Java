435ccfa328009defd7b73031cc140d40369d3942
Nullbaba <akashkolde1320@gmail.com>
2024-12-08 17:36:15 +0000

    The assignment 5 of kunal kushwaha

--- Assignment5.java
+++ Assignment5.java
@@ -1,1 +1,19 @@
 //5. Take 2 numbers as input and print the largest number.
+
+import java.util.Scanner;
+
+public class Assignment5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Number : ");
+        int n1 = sc.nextInt();
+        System.out.println("Enter the Second Number : ");
+        int n2 = sc.nextInt();
+        if (n1>n2) {
+            System.out.println("The largest number is : "+n1);
+        }else{
+            System.out.println("The largest number is : "+n2);
+        }
+        sc.close();
+    }
+}
