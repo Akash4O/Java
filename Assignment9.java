ce8cbbd73a670282a353a389008b05c4605828c6
Nullbaba <akashkolde1320@gmail.com>
2024-12-08 18:46:18 +0000

    Assignment No. 9 of Kunal Kushawa Java

---
+++ Assignment9.java
@@ -0,0 +1,34 @@
+// 9. To find Armstrong Number between two given number.
+
+import java.util.Scanner;
+
+public class Assignment9 {
+
+    public static boolean Armstrong(int num){
+        int originalNum = num;
+        int sum = 0;
+
+        int size = String.valueOf(num).length();
+
+        while(num>0){
+            int digit = num %10;
+            sum = sum + Math.pow(digit, size);
+            num = num / 10;
+        }
+
+        return sum == originalNum;
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number :");
+        int num = sc.nextInt();
+        if (Armstrong(num)) {
+            System.out.println(num+"Is an Armstrong Number.");
+        }else{
+            System.out.println(num+"Is not an Armstrong Number.");
+        }
+        sc.close();
+    }
+}
