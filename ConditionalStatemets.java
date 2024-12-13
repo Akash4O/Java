373c7ae5085ab545d03546deb099e1f9efdc4912
Akash4O <akashkolde1320@gmail.com>
2024-12-13 11:55:35 +0530

    Conditional Program

--- ConditionalStatemets.java
+++ ConditionalStatemets.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class ConditionalStatemets {
     public static void main(String[] args) {
-        
+        // Find the largeest of the 3 integers
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int max = 0;
+        // if (a>b) {
+        //     max=a;
+        // }else{
+        //     max=b;
+        // }
+
+        // if(c>max){
+        //     max=c;
+        // }
+
+        //OR
+
+        max = Math.max(c,Math.max(a, b));
+
+
+        System.out.println(max);
+        sc.close();
+
+
     }
 }
